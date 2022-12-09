68231c4fb670a5e17f5d12e6743322ffb8c7850c
ZeRX1 <kacper@zacharewicz.info>
2022-12-09 12:27:04 +0100

    fix?

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import csv
 import pandas as pd
 
 
-def dataToDataFrame(data):
+def dataToDF(data):
     tables = []
     for table in data:
         for line in table.records:
@@ -12,13 +12,12 @@ def dataToDataFrame(data):
             value_name = line.get_measurement() + "_" + line.get_field()
             tables.append(oneresulttable)
     
-    df = finishDataFrame(tables, value_name)
+    df = finishDF(tables, value_name)
     return df
 
 
-def finishDataFrame(tables, value_name):
+def finishDF(tables, value_name):
     df = pd.DataFrame(tables, columns=["_timestamp", value_name])
-
     return df
 
 def exportDFToFile(df, name):
